a0c9ebb60ebd8908bffc028999396184a3d830d6
midnighter67 <midnighter67@gmail.com>
2023-04-28 15:06:20 -0600

    !

--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -29,10 +29,10 @@ ROOT_PATH =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
