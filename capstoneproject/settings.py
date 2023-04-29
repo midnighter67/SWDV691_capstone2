32c9ff7c06cac21b36470f492cbe02269fd32f79
midnighter67 <midnighter67@gmail.com>
2023-04-28 19:13:17 -0600

    more frustration?

--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -154,7 +154,7 @@ AUTH_USER_MODEL = 'happy_home.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.yahoo.com'
 
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
