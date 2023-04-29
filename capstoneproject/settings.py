e45e84de19f5b564e5224163c7249ac8e16c374a
midnighter67 <midnighter67@gmail.com>
2023-04-28 18:42:14 -0600

    more frustration?

--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_PATH =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-config = config(RepositoryEnv(BASE_DIR))
+config = config(RepositoryEnv(".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
